3be5e130c1fd2a1ca5fca02d4350afddd8b36e17
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2021-12-11 12:44:54 -0300

    add: Estilo a home

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,5 +11,5 @@ for (let i = 0; i < cards.length; i++){
     nomeFilme = filmes[i].nome;
     imagemFilme = filmes[i].foto;
     descricaoPequena = filmes[i].descCurta;
-    cards[i].innerHTML ="<img src='./assets"+imagemFilme+"'class='card-img-top' alt=''><div class='card-body'><h1 class='card-title'>"+nomeFilme+"</h1>\n<p class='card-text'>"+descricaoPequena+"</p>\n<a href='about-item-catalog.html?filme="+i+"' class='btn btn-success'>Go somewhere</a>\n</div>";
+    cards[i].innerHTML ="<img src='./assets"+imagemFilme+"'class='card-img-top' alt=''><div class='card-body'><h1 class='card-title'>"+nomeFilme+"</h1>\n<p class='card-text'>"+descricaoPequena+"</p>\n<a href='about-item-catalog.html?filme="+i+"' class='btn btn-success'>Abrir</a>\n</div>";
 }
