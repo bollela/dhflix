73cad44050ea3c78af2ee7f7eb5f36e6608c90e3
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2021-12-11 00:35:04 -0300

    add: Estilo no about-item-catalog

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,4 +1,5 @@
 const filmes = JSON.parse(dataFilmes);
+const maxStars = 5;
 
 let pesquisa = window.location.search;
 pesquisa = pesquisa[pesquisa.length-1];
@@ -6,6 +7,26 @@ pesquisa = pesquisa[pesquisa.length-1];
 videoId = filmes[pesquisa].trailer;
 
 let movieSection = document.getElementById('movie-video');
+let movieTitle = document.getElementById('movie-title');
+let longDescription = document.getElementById('long-description');
+let director = document.getElementById('director');
+let writters = document.getElementById('writters');
+let launchDate = document.getElementById('launch-date');
+let stars = document.getElementById('stars');
+let notaFilme = 0
+let outlineStars = "";
 
-movieSection.innerHTML = "<iframe src='https://www.youtube.com/embed/"+videoId+"?autoplay=1&mute=1' allow='autoplay; clipboard-write; encrypted-media;'></iframe>";
+movieSection.innerHTML = "<iframe src='https://www.youtube.com/embed/"+videoId+"?autoplay=1&mute=1&loop=1' allow='autoplay; clipboard-write; encrypted-media;'></iframe>";
+movieTitle.innerHTML = filmes[pesquisa].nome;
+longDescription.innerHTML = filmes[pesquisa].descLonga;
+director.innerHTML = filmes[pesquisa].diretor;
+writters.innerHTML = filmes[pesquisa].roteirista;
+launchDate.innerHTML = filmes[pesquisa].data;
+
+if((filmes[pesquisa].nota == 0 ) || (filmes[pesquisa].nota == undefined)){
+    for(let i = maxStars; i > 0; i--){
+        outlineStars+="<li>☆</li>";
+    }
+    stars.innerHTML = outlineStars;
+}
 
