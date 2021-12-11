5640c957772d462f103da9e370c702f4f9e90902
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2021-12-10 21:48:11 -0300

    fix: autoplay do youtube

--- assets/js/about.js
+++ assets/js/about.js
@@ -7,21 +7,5 @@ videoId = filmes[pesquisa].trailer;
 
 let movieSection = document.getElementById('movie-video');
 
-// Load the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/player_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-// Replace the 'ytplayer' element with an <iframe> and
-// YouTube player after the API code downloads.
-var player;
-function onYouTubePlayerAPIReady() {
-player = new YT.Player(movieSection, {
-    videoId: videoId,
-    playerVars: {'autoplay': 1, 'controls':0}
-});
-}
-
-// movieSection.innerHTML = "<iframe src='https://www.youtube.com/embed/"+videoId+"?autoplay=1' allow='autoplay; clipboard-write; encrypted-media;'></iframe>";
+movieSection.innerHTML = "<iframe src='https://www.youtube.com/embed/"+videoId+"?autoplay=1&mute=1' allow='autoplay; clipboard-write; encrypted-media;'></iframe>";
 
