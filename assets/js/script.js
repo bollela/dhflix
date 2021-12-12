1155419dabc71ba1ccef31b5719f641d101d3925
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2021-12-12 00:34:00 -0300

    fix: CSS Ajusts

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ const filmes = JSON.parse(dataFilmes);
 // console.log(filmes);
 
 let cards = document.querySelectorAll('.card');
+let columnsCards = document.querySelectorAll('.col-card');
 
 let nomeFilme="";
 let imagemFilme="";
@@ -14,14 +15,10 @@ for (let i = 0; i < cards.length; i++){
     cards[i].innerHTML ="<img src='./assets"+imagemFilme+"'class='card-img-top' alt=''><div class='card-body'><h1 class='card-title'>"+nomeFilme+"</h1>\n<p class='card-text'>"+descricaoPequena+"</p>\n<a href='about-item-catalog.html?filme="+i+"' class='btn btn-success'>Abrir</a>\n</div>";
 }
 
-// const open = document.getElementById('open');
-// const close = document.getElementById('close');
-// const modal_container = document.getElementById('modal_container');
+function addFlex(e){
+    e.classList.add("d-flex");
+}
 
-// open.addEventListener('click', () => {
-//     modal_container.classList.add('show');
-// });
-
-// close.addEventListener('click', () => {
-//     modal_container.classList.remove('show')
-// })
+function removeFlex(e){
+    setTimeout(()=>e.classList.remove("d-flex"),301);
+}
